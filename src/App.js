90fb2592d92bdba3865b322a77a6030512fb6878
Adryelle Silva <191036532940@academico.ufs.br>
2024-05-07 10:57:48 -0300

    test

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './background.svg';
+import './App.css';
+import bolha1 from './bolha1.svg'
+import bolha2 from './bolha2.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={bolha1} alt="logo" />
+        <div className="descricao">
+          <h1>ssjjsjsjjsj</h1>
+        </div>
+        <img src={bolha2} alt="logo" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
