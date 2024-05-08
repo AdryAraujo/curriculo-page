2607f17ccda08d642ca5f41400fb486297516ead
Adryelle Silva <191036532940@academico.ufs.br>
2024-05-08 11:11:27 -0300

    pondo fontes do google

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ function App() {
       <div className="container">
         <img id='bolha1' src={bolha1} alt="logo" />
         <div className="descricao">
-          <h1>ssjjsjsjjsj</h1>
+          <h1>Adryelle Araujo da Silva</h1>
+          <h3>Front-end Developer | WebDsegin | Power Bi</h3>
+          <p>Estudante de Ciência da Computação pela Universidade Federal de Sergipe, desenvolvedora Web com foco em Frontend, aprendendo cada vez mais Web Design. Apaixonada em descobrir coisas novas.</p>
         </div>
         <img id='bolha2' src={bolha2} alt="logo" />
       </div>
