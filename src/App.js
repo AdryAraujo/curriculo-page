73ed689040a5802fdf6475c41d25f118c22479be
Adryelle Silva <191036532940@academico.ufs.br>
2024-05-08 09:51:22 -0300

    começando a estrutura do cabeçalho e descrição

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import logo from './background.svg';
 import './App.css';
 import bolha1 from './bolha1.svg'
 import bolha2 from './bolha2.svg'
+import perfil from './perfil.svg';
 
 function App() {
   return (
     <div className="App">
+              
+      <div className='nav'>
+        ddddddd
+
+      </div>
+      <img src={perfil} alt="logo" />
+
       <div className="container">
-        <img src={bolha1} alt="logo" />
+        {/* <img src={bolha1} alt="logo" /> */}
         <div className="descricao">
           <h1>ssjjsjsjjsj</h1>
         </div>
-        <img src={bolha2} alt="logo" />
+        {/* <img src={bolha2} alt="logo" /> */}
       </div>
     </div>
   );
